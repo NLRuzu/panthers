d7e4c32823877a5ede4536a2a7268f4d46c1739c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-03-30 13:32:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS GENERALES
 				  
 
     if (message.content.startsWith(ft + "comunicar")) {        //  +comunicar mensaje  = Enviamos un comunicado en una sala concreta
-		  if(message.member.roles.find("name", "Capitán") || (message.member.roles.find("name", "Corsario")){
+		  if(message.member.roles.find("name", "Capitán") || message.member.roles.find("name", "Corsario")){
 		  var comunicado = message.content.replace("+comunicar ", "");  
 		  let adminRoleObject = message.guild.roles.find("name", "Grumete");
 			var embebido = {
