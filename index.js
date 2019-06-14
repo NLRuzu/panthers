3617b6bd17306f7c49c7ed5a93dacf596934b188
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-06-14 17:51:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS GENERALES
                 let role = message.guild.roles.find("name", "BLOODBROTHERS");
                 let role2 = message.guild.roles.find("name", "❎ NO VERIFICADO");
                 let guild = bot.guilds.get("458220475957379074");
-                let miembro = guild.member(User);
+                let miembro = message.member(User);
                 message.miembro.addRole(role).catch(console.error);
                 message.miembro.removeRole(role2).catch(console.error);
                 User.send({
