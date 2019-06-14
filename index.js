7cfccf4b8b91e4b1456e2597d32932e9429ad130
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-06-14 19:40:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -310,7 +310,7 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS GENERALES
 	if (message.content.startsWith(ft + "verificar")) {        //  +verificar @user   = Verificamos a un usuario
             message.delete();
       if(message.member.roles.find("name", "ADMIN") || message.member.roles.find("name", "GM") || message.member.roles.find("name", "CO-GM") || message.member.roles.find("name", "OFICIAL")){
-                let User = message.mentions.users.first().catch(console.error);
+                let rUser = message.guild.member(message.mentions.users.first()).catch(console.error);
                 let role = message.guild.roles.find("name", "BLOODBROTHERS").catch(console.error);
                 let role2 = message.guild.roles.find("name", "❎ NO VERIFICADO").catch(console.error);
                 let guild = bot.guilds.get("559319996162113537").catch(console.error);
