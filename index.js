6c5f15900ee06ffa8b383ca77c0368a1442f5e11
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2020-04-08 11:23:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,52 +25,6 @@ fs.readdir("./commands/", (err, files) => {
  
 });
  
-// MENSAJE DE BIENVENIDA NUEVOS USUARIOS //
-bot.on("guildMemberAdd", async member => {
-	  
-  console.log(`${member.id} ha entrado al server `);
-            
-  let welcomechannel = member.guild.channels.find(`name`, "📈-entradas");
-    let embed = { embed: {
-                color: 0xc6ff00,
-                title: "Hola bienvenido a Ga-Meth",
-                description: '**Toda la información referente al servidor. Ve a la sala <#672782884473536522>**', 
-                
-            }
-    };
-  
-  welcomechannel.send(`**Bienvenido: Nuevo usuario ${member} a Ga-Meth**`, embed)
-
-    member.send({
-        embed: {
-                color: 0xc6ff00,
-                title: "Hola bienvenido a Ga-Meth",
-                description: '**Toda la información referente al servidor. Ve a la sala <#672782884473536522>**',   
-            }
-        });
- });
-
-// MENSAJE DE ABANDONO DE USUARIOS
-bot.on("guildMemberRemove", async member => {
-  console.log(`${member.nickname} ha abandonado el server `);
- 
-  let welcomechannel = member.guild.channels.find(`name`, "📉-abandonos");
-  welcomechannel.send({
-               embed: {
-                        color: 0xc6ff00,
-                        title: "**HA ABANDONADO**",
-		       	url: "http://gamedev.es/",
-                        description: `${member} ha abandonado la comunidad`,
-                    }
-                });
-});
-
-// ROL DE ENTRADA AL SERVIDOR - NO VERIFICADO //
-bot.on("guildMemberAdd", function(member) {
-    let role = member.guild.roles.find("name", "✅Verificado");
-    member.addRole(role).catch(console.error);
-	
-});
 
 // LISTA DE COMANDOS
 bot.on("message", (message) => {
@@ -92,17 +46,7 @@ if (message.channel.id == "672785811888013321" && message.author.bot) {
 if (message.channel.id == message.channel.id) { 				    // COMANDOS SOLO STAFF
 
 		
-	if (message.content.startsWith(ft + "serverinfo")) {       //  +serverinfo  = Muestra la informaci贸nd del servidor
-			  let sicon = message.guild.iconURL;
-			  let serverembed = new Discord.RichEmbed()
-			  .setDescription("**Informaci贸n del Servidor**")
-			  .setColor("#c6ff00")
-			  .setThumbnail(sicon)
-			  .addField("**Nombre del Servidor**", message.guild.name)
-			  .addField("**Miembros Totales**", message.guild.memberCount);
-			  
-			  return message.channel.send(serverembed);
-			  }			  
+	  
 // comunicar general
     if (message.content.startsWith(ft + "comunicar")) {        //  +comunicar mensaje  = Enviamos un comunicado en una sala concreta
 		  if(message.member.roles.find("name", "STAFF")){
@@ -136,25 +80,7 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS SOLO STAFF
 
 	
 	
-	
 
-	if (message.content.startsWith(ft + "staff")) {            //  +staff   = Informaci贸n de todos los comandos de STAFF
-		if(message.member.roles.find("name", "STAFF")){
-		  let sicon = message.guild.iconURL;
-		  let serverembed = new Discord.RichEmbed()
-		  .setDescription("**COMANDOS STAFF**")
-		  .setColor("#C6FF00")
-		  .setThumbnail(sicon)
-		  .addField("**+clear cantidad**", "Elimina X cantidad de mensajes de un canal de texto")
-		  .addField("**+comunicar Desarrollo**", "Redactamos un comunicado, solo usar en sala <#672783077700796430>")
-		  .addField("**+online**", "Publicamos un anuncio de ONLINE")
-		  .addField("**+offline**", "Publicamos un anuncio de OFFLINE")
-		  .addField("**+restart**", "Publicamos un anuncio de RESTART servidor")
-		  .addField("**+dm @user Mensaje**", "Le enviamos un DM a un usuario con un mensaje")
-		  
-		  return message.channel.send(serverembed);
-		  }
-		}
 	
 	if (message.content.startsWith(ft + "ayuda")) {            //  +staff   = Informaci贸n de todos los comandos de STAFF
 		if(message.member.roles.find("name", "✅Verificado")){
@@ -176,69 +102,7 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS SOLO STAFF
 	
     } // FIN COMANDOS STAFF
 	
-	
-	// crear clan
-	if (message.content.startsWith(ft + "crearclan")) {  //  +quedada "Nick1" "Nick2"     = Participantes del torneo
-			var args = [];
-			var texto = message.content;
-			try{
-				while(texto.includes("\"")){
-					texto = texto.substr(texto.indexOf("\"")+1);
-					args.push(texto.substring(0,texto.indexOf("\"")));
-					texto = texto.substr(texto.indexOf("\"")+1);
-				}
-			}
-			catch(err){
-				message.channel.send("+crearclan \"NombredelClan\" \"Líder\" \"Miembros\" \"Facción\"");
-				return;
-			}
-			let server = bot.guilds.get("597732937659842581");
-			let adminRoleObject = message.guild.roles.find("name", "STAFF");
-			let NickParticipante1 = args[0];
-			let NickParticipante2 = args[1];
-			let NickParticipante3 = args[2];
-			let NickParticipante4 = args[3];
-			let disponible = args[2];
-			var apuntarme = {
-				"embed": {
-					color: 0xafff00 ,
-					title: "**NUEVA SOLICITUD DE CREACIÓN DE CLAN**",
-					url: "http://gamedev.es/",				
 
-					fields: [
-						{
-						name: "📜 Nombre del Clan",
-						value: NickParticipante1,
-						},
-						{
-						name: "📜 Líder",
-						value: NickParticipante2,
-						},
-						{
-						name: "📜 Miembros",
-						value: NickParticipante3,
-						},
-						{
-						name: "📜 Facción",
-						value: NickParticipante4,
-						}
-									 						
-					]
-				}
-			};
-
-
-			let torneochannel = bot.channels.get("672894817650606130").send(`[${adminRoleObject}]`, apuntarme);
-			if(!torneochannel) return message.channel.send("No se encuentra la sala");
-
-
-			message.delete().catch(O_o=>{});
-			
-		}
-	
-	// fin crear clan
-	
-	
 	
 	
   // SISTEMA NOTIFICACIONES //
@@ -252,11 +116,11 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS SOLO STAFF
 				"title": "VOLVEMOS A ESTAR ONLINE",
 		"description": "El servidor vuelve a estar ONLINE de nuevo, disculpen las molestias y gracias por la paciencia.",
     				"footer": {
-     				"icon_url": "https://i.imgur.com/PqFyBdO.png",
-				"text": "GAMETH"
+     				"icon_url": "https://i.imgur.com/8dGnJ8c.png",
+				"text": "BandolerosRP"
   				  },
 				"image": {
-      				"url": "https://i.imgur.com/9ffNkhz.png"
+      				"url": "https://i.imgur.com/3Zhppty.png"
     				},
      				
    				"fields": [
@@ -280,11 +144,11 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS SOLO STAFF
 				"title": "REINICIANDO SERVIDOR",
 		"description": "El servidor será reiniciado, en breves podras volver a entrar y disfrutar de él. Disculpen las molestias",
     				"footer": {
-     				"icon_url": "https://i.imgur.com/PqFyBdO.png",
-				"text": "GAMETH"
+     				"icon_url": "https://i.imgur.com/8dGnJ8c.png",
+				"text": "BandolerosRP"
   				  },
 				"image": {
-      				"url": "https://i.imgur.com/p54Ljo1.png"
+      				"url": "https://i.imgur.com/BhZGxZp.png"
     				},
    				"fields": [
       				]
@@ -307,11 +171,11 @@ if (message.channel.id == message.channel.id) { 				    // COMANDOS SOLO STAFF
 				"title": "ESTAMOS EN MANTENIMIENTO",
 				"description": "El servidor en este momento se encuentra OFFLINE, disculpen las molestias y gracias por la paciencia.",
     				"footer": {
-     				"icon_url": "https://i.imgur.com/PqFyBdO.png",
-				"text": "GAMETH"
+     				"icon_url": "https://i.imgur.com/8dGnJ8c.png",
+				"text": "BandolerosRP"
   				  },
 	   		 	"image": {
-      				"url": "https://i.imgur.com/J9UQgr9.png"
+      				"url": "https://i.imgur.com/DTuySYB.png"
     				},
 
 				    "fields": [
@@ -360,7 +224,7 @@ let adminRoleObject = message.guild.roles.find("name", "STAFF");
                                   name: message.author.tag,
                                   icon_url: message.author.avatarURL
                               },
-                              title: "**NUEVA SUGERENCIA**",
+                              title: "** 💎 NUEVA SUGERENCIA 💎 **",
               url: "http://gamedev.es/",
 
               description: comunicado,
@@ -373,10 +237,10 @@ let adminRoleObject = message.guild.roles.find("name", "STAFF");
       }	
 	/// FIN SUGERENCIA ///
 	
-	// +solicitar //
-if(message.content.toUpperCase().startsWith("+SOLICITAR")){
+// +BUG //
+if(message.content.toUpperCase().startsWith("+BUG")){
 let adminRoleObject = message.guild.roles.find("name", "STAFF");	
-  var comunicado = message.content.replace("+solicitar ", "");
+  var comunicado = message.content.replace("+bug ", "");
     var embebido = {
                           "embed": {
                               color: 0xc6ff00,
@@ -384,7 +248,7 @@ let adminRoleObject = message.guild.roles.find("name", "STAFF");
                                   name: message.author.tag,
                                   icon_url: message.author.avatarURL
                               },
-                              title: "**NUEVA SOLICITUD BANDERA BLANCA**",
+                              title: "**  🧩  NUEVA REPORTE DE BUG  🧩  **",
               url: "http://gamedev.es/",
 
               description: comunicado,
@@ -392,10 +256,10 @@ let adminRoleObject = message.guild.roles.find("name", "STAFF");
                           }
                   };
 
-              bot.channels.get("672800388184801310").send(`[${adminRoleObject}]`, embebido);
+              bot.channels.get("672785811888013321").send(`[${adminRoleObject}]`, embebido);
               message.delete().catch(O_o=>{});
       }	
-	/// FIN SUGERENCIA ///
+	/// FIN BUG ///	
 	
 
    
